5932436e37a5e40b79ed024874356d35db5380ad
Sadhak Saini <sadhaksaini18@gmail.com>
2023-03-21 22:51:07 +0530

    sol6

---
+++ sol6.java
@@ -0,0 +1,9 @@
+public class sol6 {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        do{
+        System.out.println("abbey");
+        }while(a < b);
+    }
+}
